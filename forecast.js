c065bed197cca81462c3c190c75bf44e77d0e499
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-24 11:51:04 +0100

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -14,33 +14,7 @@ $.ajax({
         dataType:"JSON",
         success:function(res){  
             console.log(res);
-            //  let weather = res.list[0].main;
-            // let temp = res.main.temp;
-            // let pressure = res.main.pressure;
-            // let humidity=res.main.humidity;
-            // let Minimum=res.main.temp_min;
-            // let Maximum=res.main.temp_max;
-            // let weatherDesc=res.weather[0].description;
-            // let windspeed=res.wind.speed;
-            // let windDirection=res.wind.deg;
-       
-        // table.html(`
-        //   <thead>
-        //   <tr>
-        //   <th>Icon</th>
-        //   <th>Weather</th>
-        //   <th>Description</th>
-        //   <th>Morning <br>Temperature</th>
-        //   <th>Night<br> Temperature</th>
-        //   <th>Min.<br>Temperature</th>
-        //   <th>Max.<br>Temperature</th>
-        //   <th>Pressure</th>
-        //   <th>Humidity</th>
-        //   <th>Wind<br> Speed</th>
-        //   <th>Wind<br> Direction</th>
-        //   </tr>
-        //   </thead>
-        // `)
+          
        for(let i=0;i<res.list.length;i++){
            let table = "<tr>"; 
            table+=`<td><img src="http://openweathermap.org/img/wn/${res.list[0].weather[0].icon}.png" alt=""/></td>`;
